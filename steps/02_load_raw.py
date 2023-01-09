ab7b0e3c861bdb1377edd9baa4222df7d695b4dd
Jeremiah Hansen <jeremiah.hansen@snowflake.com>
2023-01-09 14:50:30 -0700

    First draft of more steps

--- steps/02_load_raw.py
+++ steps/02_load_raw.py
@@ -2,7 +2,7 @@
 # Hands-On Lab: Data Engineering with Snowpark
 # Script:       02_load_raw.py
 # Author:       Caleb Baechtold, Jeremiah Hansen
-# Last Updated: 1/7/2023
+# Last Updated: 1/9/2023
 #------------------------------------------------------------------------------
 
 from snowflake.snowpark import Session
---
+++ steps/09_deploy_via_cicd.sql
@@ -0,0 +1,7 @@
+/*-----------------------------------------------------------------------------
+Hands-On Lab: Data Engineering with Snowpark
+Script:       09_deploy_via_cicd.sql
+Author:       Jeremiah Hansen
+Last Updated: 1/9/2023
+-----------------------------------------------------------------------------*/
+
